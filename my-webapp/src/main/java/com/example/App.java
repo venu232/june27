c4fe9ee2db3849a24ba138654c9d7899eab4c392
root <root@ip-172-31-94-232.ec2.internal>
2025-06-29 06:18:46 +0000

    my first commit

---
+++ my-webapp/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.fusesource.jansi.AnsiConsole;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        AnsiConsole.systemInstall();
+        System.out.print(ansi().bold().fgBrightRed().a(">").fgBrightYellow().a(">").fgBrightGreen().a(">").reset().a(" "));
+        System.out.println("om namaha shivaya ");
+    }
+}
