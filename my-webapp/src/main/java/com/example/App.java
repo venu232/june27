d77745aa3f22c81e58ba2056ececeee97b44de67
root <root@ip-172-31-94-232.ec2.internal>
2025-06-29 11:52:59 +0000

    second commit

--- my-webapp/src/main/java/com/example/App.java
+++ my-webapp/src/main/java/com/example/App.java
@@ -9,6 +9,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         AnsiConsole.systemInstall();
         System.out.print(ansi().bold().fgBrightRed().a(">").fgBrightYellow().a(">").fgBrightGreen().a(">").reset().a(" "));
-        System.out.println("om namaha shivaya ");
+        System.out.println("Hello World!");
     }
 }
